71c54b600f54418f3eefb1bab85bafba4d0ed3c8
Santosshah <sanotsh_shah25@hotmail.com>
2022-10-02 18:32:44 +0545

    add lastname

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -5,7 +5,7 @@ const Data = () => {
   return (
     <div className="home__data">
       <h1 className="home__title">
-        Santosh
+        Santosh shah
         <svg
           width="36"
           height="36"
