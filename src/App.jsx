38e3cb849dff3a06c0398984b9288d26365bc68b
Santosshah <sanotsh_shah25@hotmail.com>
2022-10-06 16:51:27 +0545

    change

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,13 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
-// import Skills from "./components/skills/Skills";
-// import Services from "./components/services/Services";
 import Qualification from "./components/qualification/Qualification";
 import Testimonials from "./components/testmonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import Scrollup from "./components/scrollup/Scrollup";
 import Project from "./components/project/Project";
-import Skills from "./components/New_Skills/Skills";
-
+import Skills from "./components/skills/Skills";
 function App() {
   return (
     <>
